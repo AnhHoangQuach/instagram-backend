722216fa15d160c37e717580b1b33c59cf890837
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-11-01 00:56:55 +0700

    feat: config login facebook app

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const authRouter = express.Router();
 
 const { signup, login } = require('../controllers/authController');
@@ -7,4 +8,13 @@ authRouter.post('/signup', signup);
 
 authRouter.post('/login', login);
 
+authRouter.get('/facebook', passport.authenticate('facebook', { session: false, scope: 'email' }));
+
+authRouter.get(
+  '/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
+  function (req, res) {
+    // save user and return jwt
+  }
+);
 module.exports = authRouter;
