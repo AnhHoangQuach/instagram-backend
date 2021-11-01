3b1bcc9b7542eb0783b1487a6ea138509c2b637f
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-11-02 00:51:33 +0700

    fix: deploy when add require package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectToDb = require('./utils/db');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const passport = require('passport');
