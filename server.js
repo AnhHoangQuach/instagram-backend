8aa7f7e3ac4cd09ff92e3c03a0648079b9dcda33
AnhHoangQuach <hoanganh36.work@gmail.com>
2022-02-11 06:57:52 +0700

    fix: cors

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ const User = require('./models/User');
 const apiRouter = require('./routes');
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 app.use(passport.initialize());
 app.use(express.json());
 
