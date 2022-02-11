4601313bd6f1acb3814e6277db9641255b7d056e
AnhHoangQuach <hoanganh36.work@gmail.com>
2022-02-11 07:10:59 +0700

    fix

--- server.js
+++ server.js
@@ -27,14 +27,9 @@ const apiRouter = require('./routes');
 
 app.use(cors());
 
-app.use(function (req, res, next) {
+app.all('/', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
